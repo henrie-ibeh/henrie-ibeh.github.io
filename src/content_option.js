9d71138f03cc51b27c35333e5b2a244ea180f5e7
henrie-ibeh <ibehhenrie@gmail.com>
2024-05-19 11:19:57 +0100

    portfolio update

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+import "../src/assets/images/img.png"
+const logotext = "HENRY";
+const meta = {
+    title: "Henry Ibeh",
+    description: "I’m Henry Ibeh data scientist _ Full stack devloper,currently working in Nigeria",
+};
+
+const introdata = {
+    title: "I’m Henry Ibeh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Data Engineer | Data Analyst | Google Cloud | Health Informatics | Python | SQL",
+    your_img_url: "https://media.licdn.com/dms/image/D4D03AQHoWO7QDsWN5A/profile-displayphoto-shrink_800_800/0/1678115626813?e=1721260800&v=beta&t=byhqznAIRbGE_OvagrQGMTBp1mrYaYl92yCp9sOgzW4",
+};
+
+const dataabout = {
+    title: "a bit about myself",
+    aboutme: "I am a person who loves technology and would like to work withcomputers. I have worked with computers since the age of 8. Thisis an ever changing field and I look forward to learning and growingin it. My goals are to learn as much as I can and then share myknowledge and experience with others.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Graphics Design",
+        description: "I would create outstanding graphics designs for you using canva.",
+    },
+    {
+        title: "Data Analysis",
+        description: "Leveraging SQL, Excel, Python, I can analyze and visualize data. ",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ibehhenrie@gmail.com",
+    YOUR_FONE: "+2348102680717",
+    description: "Feel free to contact me at anytime. Lets talk about how my skill could improve your Company's needs.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/henrie-ibeh",
+    facebook: "https://facebook.com/ibehuk",
+    linkedin: "https://www.linkedin.com/in/henry-ibeh-971061197/",
+    X: "https://x.com/henriefloat",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
